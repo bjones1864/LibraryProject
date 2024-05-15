1b718091d81e304b7d4aec0c0fc6141e370492aa
Ben Jones <benjones1864@gmail.com>
2024-05-15 14:03:25 -0400

    first commit

---
+++ LibraryProject/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LibraryProject
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsOnShelf { get; set; }
+        public string DueDate { get; set; }
+
+        public Book(string _title, string _author, bool _isOnShelf, string _dueDate)
+        {
+            Title = _title;
+            Author = _author;
+            IsOnShelf = _isOnShelf;
+            DueDate = _dueDate;
+        }
+
+    }
+}
